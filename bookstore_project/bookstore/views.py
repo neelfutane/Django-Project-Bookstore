4abed7e15d4466e454f5f472f36edace870367a1
neelfutane <neelgit64@gmail.com>
2025-04-22 23:31:45 +0530

    Update views.py

--- bookstore_project/bookstore/views.py
+++ bookstore_project/bookstore/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from .models import Book 
 
@@ -20,13 +21,31 @@ def register_view(request):
 from django.contrib import messages
 from django.contrib.auth import logout
 
+# def login_view(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('book_list')
+#         else:
+#             messages.error(request, "Invalid username or password")
+
+#     return render(request, 'bookstore/login.html')
+
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '')
+
+        if not username or not password:
+            messages.error(request, "Both username and password are required.")
+            return render(request, 'bookstore/login.html')
 
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user:
             login(request, user)
             return redirect('book_list')
         else:
