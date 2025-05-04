d932b55c64a2dee56600713a324228656343e0d1
neelfutane <neelgit64@gmail.com>
2025-05-04 18:11:37 +0530

    Update urls.py

--- bookstore_project/bookstore/urls.py
+++ bookstore_project/bookstore/urls.py
@@ -1,16 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#      path('books/', views.book_list_view, name='book_list'),
-#     path('', views.home_view, name='home'),
-#     path('login/', views.login_view, name='login'),
-#     path('logout/', views.logout_view, name='logout'),
-#     path('add-to-cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
-#     path('cart/', views.cart_view, name='cart_view'),
-#      path('register/', views.register_view, name='register')
-# ]
-
 from django.urls import path
 from .views import (
     RegisterView, LoginView, LogoutView, HomeView,
