7b3dd757932f82a284db00f5630365dc13998673
neelfutane <neelgit64@gmail.com>
2025-05-04 17:20:02 +0530

    Update urls.py

--- bookstore_project/bookstore/urls.py
+++ bookstore_project/bookstore/urls.py
@@ -1,12 +1,28 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#      path('books/', views.book_list_view, name='book_list'),
+#     path('', views.home_view, name='home'),
+#     path('login/', views.login_view, name='login'),
+#     path('logout/', views.logout_view, name='logout'),
+#     path('add-to-cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
+#     path('cart/', views.cart_view, name='cart_view'),
+#      path('register/', views.register_view, name='register')
+# ]
+
 from django.urls import path
-from . import views
+from .views import (
+    RegisterView, LoginView, LogoutView, HomeView,
+    BookListView, AddToCartView, CartView
+)
 
 urlpatterns = [
-     path('books/', views.book_list_view, name='book_list'),
-    path('', views.home_view, name='home'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('add-to-cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart_view, name='cart_view'),
-     path('register/', views.register_view, name='register')
+    path('', HomeView.as_view(), name='home'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('books/', BookListView.as_view(), name='book_list'),
+    path('add-to-cart/<int:book_id>/', AddToCartView.as_view(), name='add_to_cart'),
+    path('cart/', CartView.as_view(), name='cart_view'),
 ]
