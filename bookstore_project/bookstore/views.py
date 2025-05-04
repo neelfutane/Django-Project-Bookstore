b9a782d3817d390631631f8df6473b63a034c2df
neelfutane <neelgit64@gmail.com>
2025-05-04 17:19:08 +0530

    Update views.py

--- bookstore_project/bookstore/views.py
+++ bookstore_project/bookstore/views.py
@@ -1,63 +1,136 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from .models import Book 
-from django.contrib import messages
-from django.contrib.auth import logout
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.models import User
+# from django.contrib.auth import login, authenticate
+# from .models import Book 
+# from django.contrib import messages
+# from django.contrib.auth import logout
 
-def register_view(request):
-    if request.method == 'POST':
+# def register_view(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         email = request.POST['email']
+#         password = request.POST['password']
+
+#         user = User.objects.create_user(username=username, email=email, password=password)
+#         user.save()
+
+#         login(request, user)
+#         return redirect('book_list') 
+
+#     return render(request, 'bookstore/register.html')
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('book_list')
+#         else:
+#             messages.error(request, "Invalid username or password")
+
+#     return render(request, 'bookstore/login.html')
+
+
+# def book_list(request):
+#     return render(request, 'book_list.html') 
+
+# def logout_view(request):
+#     logout(request)
+#     return redirect('login')
+
+# def add_to_cart(request, book_id):
+#     cart = request.session.get('cart', [])
+#     if book_id not in cart:
+#         cart.append(book_id)
+#     request.session['cart'] = cart
+#     return redirect('book_list')
+
+# def cart_view(request):
+#     cart = request.session.get('cart', [])
+#     books = Book.objects.filter(id__in=cart)
+#     total_price = sum(book.price for book in books)
+#     return render(request, 'bookstore/cart.html', {'cart_books': books ,'total_price': total_price,})
+
+# def home_view(request):
+#     return render(request, 'home.html')
+
+
+# def book_list_view(request):
+#     books = Book.objects.all()
+#     return render(request, 'bookstore/book_list.html', {'books': books})
+
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView, TemplateView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Book
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'bookstore/register.html')
+
+    def post(self, request):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
 
         user = User.objects.create_user(username=username, email=email, password=password)
-        user.save()
-
         login(request, user)
-        return redirect('book_list') 
+        return redirect('book_list')
 
-    return render(request, 'bookstore/register.html')
 
-def login_view(request):
-    if request.method == 'POST':
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'bookstore/login.html')
+
+    def post(self, request):
         username = request.POST['username']
         password = request.POST['password']
-
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+
+        if user:
             login(request, user)
             return redirect('book_list')
         else:
             messages.error(request, "Invalid username or password")
-
-    return render(request, 'bookstore/login.html')
-
-
-def book_list(request):
-    return render(request, 'book_list.html') 
-
-def logout_view(request):
-    logout(request)
-    return redirect('login')
-
-def add_to_cart(request, book_id):
-    cart = request.session.get('cart', [])
-    if book_id not in cart:
-        cart.append(book_id)
-    request.session['cart'] = cart
-    return redirect('book_list')
-
-def cart_view(request):
-    cart = request.session.get('cart', [])
-    books = Book.objects.filter(id__in=cart)
-    total_price = sum(book.price for book in books)
-    return render(request, 'bookstore/cart.html', {'cart_books': books ,'total_price': total_price,})
-
-def home_view(request):
-    return render(request, 'home.html')
+            return render(request, 'bookstore/login.html')
 
 
-def book_list_view(request):
-    books = Book.objects.all()
-    return render(request, 'bookstore/book_list.html', {'books': books})
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'bookstore/book_list.html'
+    context_object_name = 'books'
+
+
+class AddToCartView(View):
+    def get(self, request, book_id):
+        cart = request.session.get('cart', [])
+        if book_id not in cart:
+            cart.append(book_id)
+        request.session['cart'] = cart
+        return redirect('book_list')
+
+
+class CartView(View):
+    def get(self, request):
+        cart = request.session.get('cart', [])
+        books = Book.objects.filter(id__in=cart)
+        total_price = sum(book.price for book in books)
+        return render(request, 'bookstore/cart.html', {'cart_books': books, 'total_price': total_price})
+
